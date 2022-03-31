d1bc525180d336b858db410493a16d73fb191f15
Beto1821 <beto1821@uol.com.br>
2022-03-31 16:36:17 -0300

    ex03 rev01

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const btn = document.getElementById('entrar');
 const email = document.getElementById('login');
 const senha = document.getElementById('password');
 
-function valida(msg) {
-  if (email.value !== 'tryber@teste.com' || senha.value !== '123456') {
+function valida() {
+  if (email.value !== 'tryber@teste.com' && senha.value !== '123456') {
     alert('Email ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
-  return msg;
 }
 
 btn.addEventListener('click', valida);
