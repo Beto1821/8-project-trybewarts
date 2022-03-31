ae15ae454da02867117308e9df3f5f0ceecd3105
lucaslscunha <lucaslscunha@gmail.com>
2022-03-31 19:55:17 -0300

    (req 18 rev 01 e req 19)

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ function valida() {
 }
 
 btn.addEventListener('click', valida);
+
+// const submitButton = document.getElementById('submit-btn');
+// const check = document.getElementById('agreement');
+// submitButton.disable = true;
+
+// check.addEventListener('click', () => {
+//   if (check.select === true) {
+//     submitButton.disable = false;
+//   } else {
+//     submitButton.disable = true;
+//   }
+// });
+
+const botao = document.getElementById('submit-btn');
+const check = document.getElementById('agreement');
+botao.disabled = true;
+
+check.addEventListener('change', () => {
+  if (check.checked) {
+    botao.disabled = false;
+  } else {
+    botao.disabled = true;
+  }
+});
