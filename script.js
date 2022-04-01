796772a0de847b925ad7e02d52e0e73fe78405c7
Beto1821 <beto1821@uol.com.br>
2022-04-01 20:12:14 -0300

    req21 prototipo

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const textArea = document.getElementById('textarea');
 const textoContador = document.getElementById('counter');
 const botao = document.getElementById('submit-btn');
 const check = document.getElementById('agreement');
+const formulario = document.getElementById('evaluation-form');
 
 function valida() {
   if (email.value !== 'tryber@teste.com' && senha.value !== '123456') {
@@ -32,3 +33,69 @@ function contador() {
 }
 
 textArea.addEventListener('input', contador);
+
+// requsisito 21
+
+function pNomeComp() {
+  const nome = document.querySelector('#input-name').value;
+  const sobren = document.querySelector('#input-lastname').value;
+
+  return `Nome: ${nome} ${sobren}`;
+}
+
+function pEmail() {
+  const email = document.getElementById('input-email').value;
+
+  return `email: ${email}`;
+
+}
+
+function pCasa() {
+  const casa = document.getElementById('house').value;
+
+  return `Casa: ${casa}`;
+}
+
+function pFamilia() {
+  const familia = document.querySelector('input[name="family"]:checked').value;
+
+  return `Familia: ${familia}`;
+}
+// https://pt.stackoverflow.com/questions/83463/pegar-valores-checkbox-com-javascript
+function pMaterias() {
+  const lMaterias = [];
+  const materias = document.getElementsByName('materia');
+  for (let m = 0; m < materias.length; m += 1) {
+    if (materias[m].checked) {
+      lMaterias.push(materias[m].value);
+    }
+  }
+  return `Matérias: ${lMaterias}`;
+}
+
+function pNota() {
+  const nota = document.querySelector('input[name="rate"]:checked').value;
+
+  return `Avaliação: ${nota}`;
+}
+
+function pObs() {
+  const obs = document.getElementById('texarea').value;
+
+  return `Observações: ${obs}`;
+}
+
+
+
+function registro() {
+const materias = document.getElementById('label-content').value;
+const avaliacao = document.getElementById('avalia').value;
+
+function clean() {
+
+}
+
+}
+
+
+botao.addEventListener('click', enviar);
